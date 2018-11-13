a6da4afff2651f827318d6a6af2451c85278b14a
Andrew Castillo <andrewcastillo@ADC-MacBook-Air.local>
2018-11-13 14:09:09 -0500

    Started the SysOpenfile array #1

--- filesys/SysOpenFile.cc
+++ filesys/SysOpenFile.cc
@@ -10,4 +10,20 @@ SysOpenFile::SysOpenfile(int fileID, char* fileName, OpenFile* file)
 	//convert int to string and create new char obj
 	newName = new char[strlen(fileID)+1];
 	strcpy(newName, fileName);
+	//# of processes using file
+	//procCount = 
+	//a file needs to be opeend or already open
+	if(fileName exists in SysOpenArray){
+		//file already open
+		printf('File already open');
+	}
+	else{
+		//look at openfile.h for sector
+		Openfile(sector);
+	}
+}
+
+SysOpenFile::OpenFileArray(char *fileName)
+{
+	
 }

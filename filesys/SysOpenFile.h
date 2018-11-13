047a917135bcca7d81774541665ab98ec7c9aa8c
ealfa009@fiu.edu <wow.Github>
2018-11-13 13:08:30 -0600

    small changes

--- filesys/SysOpenFile.h
+++ filesys/SysOpenFile.h
@@ -8,7 +8,9 @@
 
 #include "copyright.h"
 #include "openfile.h"
-
+/*
+A system's open file class: Needs to contain information such as the filename, an OpenFile object, and the number of processes that have this file open. The OpenFile object is what will be used to actually read and write to this file. You can look at this in filesys/openfile.h directory where the stubs are contained. The number of process with the file open will be helpful to determine if a file really can be closed or not because other processes are still using it. It will also determine if a file needs to be opened or if it is already open.
+*/
 
 class SysOpenFile {
 	public:
@@ -18,5 +20,6 @@ class SysOpenFile {
 		int procCount;
 		char *newName;
 		OpenFile* openFile;
+		SysOpenFile* file[50];
 };
 #endif SysOpenFile_H

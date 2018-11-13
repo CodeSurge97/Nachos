0840d2fec4dc0da05082b2e7746e6962185bcfff
Andrew Castillo <andrewcastillo@ADC-MacBook-Air.local>
2018-11-13 16:17:04 -0500

    Finish #1-#2

--- filesys/SysOpenFile.h
+++ filesys/SysOpenFile.h
@@ -6,17 +6,19 @@ Class that contains pointer to the file system's openfile object as well as the 
 #ifndef SysOpenFile_H
 #define SysOpenFile_H
 
-#define ARRAY_SIZE 32;
+#define ARRAY_SIZE 32
 
 #include "copyright.h"
 #include "openfile.h"
+#include "bitmap.h"
+
 /*
 A system's open file class: Needs to contain information such as the filename, an OpenFile object, and the number of processes that have this file open. The OpenFile object is what will be used to actually read and write to this file. You can look at this in filesys/openfile.h directory where the stubs are contained. The number of process with the file open will be helpful to determine if a file really can be closed or not because other processes are still using it. It will also determine if a file needs to be opened or if it is already open.
 */
 
 class SysOpenFile {
 	public:
-		SysOpenfile(int FileID, char fileName, OpenFile *file)
+		SysOpenFile(int fileID, char* fileName, OpenFile *file);
 		~SysOpenFile();
 		int id;
 		int procCount;
@@ -24,8 +26,11 @@ class SysOpenFile {
 		OpenFile* openFile;
 		SysOpenFile* file[ARRAY_SIZE];
 		bool OpenFileArray(char *fileName);
-		bool clearID(int i);
+		bool ClearID(int i);
+		int GetID();
+		
 	private:
-		BitMap* mapFiles; 
+		BitMap* mapFiles;
+		Lock* memLock; 
 };
 #endif SysOpenFile_H
